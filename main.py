6658e657670938cd61a720fb04627e11312e0322
kWAY <55437151+kWAYTV@users.noreply.github.com>
2023-05-10 21:19:16 +0200

    Added config file and i handle as much errors as i find

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import json, os, requests, logging
+import json, os, requests, logging, yaml, re
 from time import sleep
+from datetime import timedelta
+from yaml import SafeLoader
 from fake_useragent import UserAgent
 from pystyle import Colors, Colorate, Center
 
+# Logo.
 logo = """
 ██╗  ██╗███████╗██╗   ██╗██████╗ ██████╗  ██████╗ ██████╗ 
 ██║ ██╔╝██╔════╝╚██╗ ██╔╝██╔══██╗██╔══██╗██╔═══██╗██╔══██╗
@@ -11,14 +14,44 @@ logo = """
 ██║  ██╗███████╗   ██║   ██████╔╝██║  ██║╚██████╔╝██║     
 ╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝"""
 
+# Default config file.
+default_config = """
+# Keydrop Bot Config
+bearer_token: ""
+sleep_interval: 1
+ticket_cost_threshold: 1000
+ratelimit_sleep: 15
+"""
+
 # Clear the console.
 clear = lambda: os.system("cls" if os.name in ("nt", "dos") else "clear")
 
 # Set the console title.
 os.system(f"title Keydrop Battle Bot - discord.gg/kws")
 
+class Config():
+    def __init__(self):
+
+        if not os.path.exists("config.yaml"):
+            logging.error("Config file not found! Creating one for you...")
+            with open("config.yaml", "w") as file:
+                file.write(default_config)
+            logging.info("Config file created! Please fill in the config file and restart the bot.")
+            exit()
+
+        with open("config.yaml", "r") as file:
+            self.config = yaml.load(file, Loader=SafeLoader)
+            self.bearer_token = self.config["bearer_token"]
+            self.sleep_interval = self.config["sleep_interval"]
+            self.ticket_cost_threshold = self.config["ticket_cost_threshold"]
+            self.ratelimit_sleep = self.config["ratelimit_sleep"]
+
+# Load the config.
+configData = Config()
+
 class CaseBattle:
-    def __init__(self, token, sleep_interval=1, ticket_cost_threshold=4):
+    # Initialize the class.
+    def __init__(self, token, sleep_interval=configData.sleep_interval, ticket_cost_threshold=configData.ticket_cost_threshold):
         self.session = requests.Session()
         self.user_agent = UserAgent()
         self.session.headers.update({
@@ -38,11 +71,13 @@ class CaseBattle:
         self.sleep_interval = sleep_interval
         self.ticket_cost_threshold = ticket_cost_threshold
 
+    # Function to print the logo.
     def print_logo(self):
         print(Center.XCenter(Colorate.Vertical(Colors.white_to_blue, logo, 1)))
         print(Center.XCenter(Colorate.Vertical(Colors.white_to_blue, "────────────────────────────────────────────\n", 1)))
         print(Center.XCenter(Colorate.Vertical(Colors.white_to_blue, "Starting...", 1)))
 
+    # Function to get active battles.
     def get_active_battles(self):
         try:
             response = self.session.get(self.active_battles_url)
@@ -55,28 +90,33 @@ class CaseBattle:
             logging.error(f"Other error occurred: {err}")
             return []
 
+    # Function to join a battle.
     def join_battle(self, battle_id):
         try:
             url = f"{self.join_battle_url}{battle_id}/1"
             response = self.session.post(url)
             response.raise_for_status()
-            json = json.loads(response.text)
-            if json["success"]:
+            data = json.loads(response.text)
+            if data["success"]:
                 return True, response.text
-            if json["errorCode"] == "slotUnavailable":
-                return False, "Slot unavailable!"
-            if json["errorCode"] == "rateLimited":
+            if data["errorCode"] == "slotUnavailable":
+                return False, "Battle is full!"
+            if data["errorCode"] == "rateLimited":
                 return False, "Ratelimited!"
-            return False, json["errorCode"]
+            if data["errorCode"] == "userHasToWaitBeforeJoiningFreeBattle":
+                return False, "You have to wait one day between free battles!"
+            print(data)
+            return False, data["errorCode"]
         except requests.HTTPError as http_err:
             if "Unauthorized" in str(response.text):
-                return False, "Invalid token!"
+                return False, "Invalid bearer token!"
             logging.error(f"HTTP Error: {http_err}")
             return False, str(http_err)
         except Exception as err:
             logging.error(f"Error: {err}")
             return False, str(err)
 
+    # Function to monitor active battles and join them if they are joinable.
     def monitor_battles(self):
         clear()
         self.print_logo()
@@ -92,13 +132,18 @@ class CaseBattle:
                         logging.error("Invalid bearer token!")
                         exit()
                     elif message == "Ratelimited!":
-                        logging.error("Ratelimited! Consider increasing the sleep interval.")
-                        sleep(60)
+                        logging.error("Ratelimited! Consider increasing the sleep interval or use proxies.")
+                        logging.info("Sleeping for 30 seconds...")
+                        sleep(30)
+                    elif message == "You have to wait one day between free battles!":
+                        logging.error("You have to wait one day between free battles!")
+                        exit()
                     else:
                         logging.error(f"Failed to join battle! Error: {message}")
                         sleep(self.sleep_interval)
             sleep(self.sleep_interval)
 
+    # Function to check if a battle is joinable.
     def is_joinable(self, battle):
         isFreeBattle = battle["isFreeBattle"]
         users = battle["users"]
@@ -110,8 +155,8 @@ class CaseBattle:
                 return True
         return False
 
+# Start the script.
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    token = "YOUR_TOKEN_HERE"
-    cb = CaseBattle(token)
+    cb = CaseBattle(configData.bearer_token)
     cb.monitor_battles()
